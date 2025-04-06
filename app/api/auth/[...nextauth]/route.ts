2e0780b1ef646d941dd3a7f62fc2e422de415c8e
SUDEEPTA SARKAR <sudsarkar13@gmail.com>
2025-04-06 22:22:08 +0530

    refactor ♻️: restrict auth options access
    - The main goal of the changes is to make the auth options constantprivate by removing the export keyword to restrict its access.

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -5,7 +5,7 @@ if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
 	throw new Error("Missing Google OAuth credentials");
 }
 
-export const authOptions: NextAuthOptions = {
+const authOptions: NextAuthOptions = {
 	providers: [
 		GoogleProvider({
 			clientId: process.env.GOOGLE_CLIENT_ID,
