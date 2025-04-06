42082ef8b00e99d88171a103b0b8d0bd7942acb5
SUDEEPTA SARKAR <sudsarkar13@gmail.com>
2025-04-06 21:24:57 +0530

    feat ✨: add session provider component
    - Added a new session provider component to support user session management with NextAuth.

---
+++ providers/SessionProvider.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import { SessionProvider as NextAuthSessionProvider } from "next-auth/react";
+import { RNChildProp } from "../@types/interface/Interface";
+
+export function SessionProvider({ children }: RNChildProp) {
+	return <NextAuthSessionProvider>{children}</NextAuthSessionProvider>;
+}
