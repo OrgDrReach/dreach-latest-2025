9ea5e378abc3b81221734653a59aef9f101e7899
SUDEEPTA SARKAR <sudsarkar13@gmail.com>
2025-04-06 22:23:31 +0530

    feat ✨: added theming functionality
    - Added theming functionality with system theme support to the layout component using the ThemeProvider2.

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,11 +1,21 @@
 import { RNChildProp } from "@/@types/interface/Interface";
 import React from "react";
 import { ubuntu, ubuntuMono } from "@/@types/font/Font";
+import { Providers } from "../providers";
+import { ThemeProvider2 } from "@/components/themes/theme-provider";
 
-export function layout({children}: RNChildProp){
-  return(
-    <main className={`${ubuntu.className} ${ubuntuMono.className} antialiased`}>
-      <div>{children}</div>
-    </main>
-  )
+export function layout({ children }: RNChildProp) {
+	return (
+		<main className={`${ubuntu.className} ${ubuntuMono.className} antialiased`}>
+			<ThemeProvider2
+				attribute="class"
+				defaultTheme="system"
+				enableSystem
+				disableTransitionOnChange>
+				<Providers>
+					<div>{children}</div>
+				</Providers>
+			</ThemeProvider2>
+		</main>
+	);
 }
