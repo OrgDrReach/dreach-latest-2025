c7071cdfb9c1dbb44db433216f89c349b9fa69bd
SUDEEPTA SARKAR <sudsarkar13@gmail.com>
2025-04-08 01:10:54 +0530

    style 💎: Improve code formatting and readability
    - There are no significant changes to the overall functionality as the modifications were essentially null, with old content being replaced by identical new content.
    - The main goal of these changes was to improve the formatting and readability of the code without changing its functionality.
    - The main goal of the changes is to refactor and format the existing layout component code for better readability and consistency.

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -5,17 +5,18 @@ import { Providers } from "../providers";
 import { ThemeProvider2 } from "@/components/themes/theme-provider";
 
 export default function DashboardLayout({ children }: RNChildProp) {
-	return (
-		<main className={`${ubuntu.className} ${ubuntuMono.className} antialiased`}>
-			<ThemeProvider2
-				attribute="class"
-				defaultTheme="system"
-				enableSystem
-				disableTransitionOnChange>
-				<Providers>
-					<div>{children}</div>
-				</Providers>
-			</ThemeProvider2>
-		</main>
-	);
+  return (
+    <main className={`${ubuntu.className} ${ubuntuMono.className} antialiased`}>
+      <ThemeProvider2
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <Providers>
+          <div>{children}</div>
+        </Providers>
+      </ThemeProvider2>
+    </main>
+  );
 }
