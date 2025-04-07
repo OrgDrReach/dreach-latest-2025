230225d48e473bd42efa6329c980d80924b31c9a
SUDEEPTA SARKAR <sudsarkar13@gmail.com>
2025-04-07 21:10:29 +0530

    feat ✨: Add about us page with hero section
    - Created a new about us page with a hero section and an overview of the company, utilizing components to showcase the main details and purpose of the business and organization.

---
+++ app/about/page.tsx
@@ -0,0 +1,20 @@
+import WhoWeAre from "@/components/page-components/about/WhoWeAre";
+
+export default function AboutUs() {
+	return (
+		<main>
+			<div className={`about`}>
+				<div className={`about-hero`}>
+					<div>
+						<h1>Welcome to Dr. Reach</h1>
+						<p>Revolutionizing healthcare management for a better tomorrow</p>
+					</div>
+				</div>
+			</div>
+			{/* stacked about components */}
+			<div>
+				<WhoWeAre />
+			</div>
+		</main>
+	);
+}
