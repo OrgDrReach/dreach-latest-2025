f8b89a93ad9f5ee9319314c5f5f0c3696885309e
SUDEEPTA SARKAR <sudsarkar13@gmail.com>
2025-04-08 01:16:23 +0530

    refactor ♻️: minor code adjustments
    - No significant changes were made to the functionality of the code, it seems to be a refactoring without introducing new features, only minor adjustments.

--- app/page.tsx
+++ app/page.tsx
@@ -9,25 +9,25 @@ import DoctorCTA from "@/components/landing/doctor/DoctorCTA";
 import ServiceSummary from "@/components/landing/service/serviceSummary";
 
 export const metadata: Metadata = {
-	title: "Dreach.in",
-	description:
-		"Dreach.in is a platform for doctors and patients to connect and communicate.",
+  title: "Dreach.in",
+  description:
+    "Dreach.in is a platform for doctors and patients to connect and communicate.",
 };
 
 const Home: React.FC = () => {
-	return (
-		<main>
-			<div>
-				<Hero />
-				<InstantAppointment />
-				<StartupIndia />
-				<About />
-				<DoctorCTA />
-				<ServiceSummary />
-				<Banner />
-			</div>
-		</main>
-	);
+  return (
+    <main>
+      <div>
+        <Hero />
+        <InstantAppointment />
+        <StartupIndia />
+        <About />
+        <DoctorCTA />
+        <ServiceSummary />
+        <Banner />
+      </div>
+    </main>
+  );
 };
 
 export default Home;
