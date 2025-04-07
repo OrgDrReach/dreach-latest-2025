3c22cbd5424053bd547e42596ffe2564411d2f8e
SUDEEPTA SARKAR <sudsarkar13@gmail.com>
2025-04-08 01:12:36 +0530

    style 💎: added newline for formatting
    - Added a newline character to the end of the file to ensure proper formatting.
    - The main goal of the changes is to implement no functional changes.

--- providers/SessionProvider.tsx
+++ providers/SessionProvider.tsx
@@ -4,5 +4,5 @@ import { SessionProvider as NextAuthSessionProvider } from "next-auth/react";
 import { RNChildProp } from "../@types/interface/Interface";
 
 export function SessionProvider({ children }: RNChildProp) {
-	return <NextAuthSessionProvider>{children}</NextAuthSessionProvider>;
+  return <NextAuthSessionProvider>{children}</NextAuthSessionProvider>;
 }
