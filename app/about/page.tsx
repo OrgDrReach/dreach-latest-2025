8f51ff76700342b04d5d57722d92b6485a03337c
SUDEEPTA SARKAR <sudsarkar13@gmail.com>
2025-04-08 13:09:12 +0530

    style 💎: update import case sensitivity
    - The main goal of the changes is to update the case sensitivity of an import statement to ensure consistency with the actual folder name location.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,24 +1,24 @@
-import FounderSection from "@/components/page-components/about/Leadership/FounderSection";
+import FounderSection from "@/components/page-components/about/leadership/FounderSection";
 import OurMissionVision from "@/components/page-components/about/mission-vision/OurMissionVision";
 import WhoWeAre from "@/components/page-components/about/whoweare/WhoWeAre";
 
 export default function AboutUs() {
-  return (
-    <main>
-      <div className={`about`}>
-        <div className={`about-hero`}>
-          <div>
-            <h1>Welcome to Dr. Reach</h1>
-            <p>Revolutionizing healthcare management for a better tomorrow</p>
-          </div>
-        </div>
-      </div>
-      {/* stacked about components */}
-      <div>
-        <WhoWeAre />
-        <OurMissionVision />
-        <FounderSection />
-      </div>
-    </main>
-  );
+	return (
+		<main>
+			<div className={`about`}>
+				<div className={`about-hero`}>
+					<div>
+						<h1>Welcome to Dr. Reach</h1>
+						<p>Revolutionizing healthcare management for a better tomorrow</p>
+					</div>
+				</div>
+			</div>
+			{/* stacked about components */}
+			<div>
+				<WhoWeAre />
+				<OurMissionVision />
+				<FounderSection />
+			</div>
+		</main>
+	);
 }
