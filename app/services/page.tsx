cc89b115891b48c9d224055baf6494a5ebac854a
SUDEEPTA SARKAR <sudsarkar13@gmail.com>
2025-04-08 23:07:47 +0530

    feat ✨: add online consultation components
    - The main goal of the changes is to add new components to the page for offering online consultation and providing more service context information to users.

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,5 +1,8 @@
 import ServiceHero from "@/components/page-components/services/hero/ServiceHero";
+import OnlineConsultation from "@/components/page-components/services/online-consultation/OnlineConsultation";
 import OurServices from "@/components/page-components/services/our-services/OurServices";
+import LearnMore from "@/components/page-components/services/service-context/LearnMore";
+import ServiceContext from "@/components/page-components/services/service-context/ServiceContext";
 import React from "react";
 
 export default function page() {
@@ -8,6 +11,9 @@ export default function page() {
 			<div>
 				<ServiceHero />
 				<OurServices />
+				<ServiceContext />
+				<LearnMore />
+				<OnlineConsultation />
 			</div>
 		</main>
 	);
