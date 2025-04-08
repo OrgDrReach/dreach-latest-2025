6c2371f2b7d203791401cad04768a6b0ecf1f6d4
SUDEEPTA SARKAR <sudsarkar13@gmail.com>
2025-04-08 21:32:08 +0530

    feat ✨: add new react page component
    - The main goal of the changes is to add a new react page component.

---
+++ app/services/page.tsx
@@ -0,0 +1,12 @@
+import ServiceHero from "@/components/page-components/services/hero/ServiceHero";
+import React from "react";
+
+export default function page() {
+	return (
+		<main>
+			<div>
+				<ServiceHero />
+			</div>
+		</main>
+	);
+}
